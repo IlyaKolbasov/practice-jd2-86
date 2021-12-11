695af55004d39e51f791636ef7147651769c6c9b
IlyaKolbasov <51168504+IlyaKolbasov@users.noreply.github.com>
2021-12-11 21:48:52 +0300

    Add files via upload

---
+++ src/main/java/byKolbis/DivisionByZeroException.java
@@ -0,0 +1,10 @@
+package byKolbis;
+
+public class DivisionByZeroException extends Exception
+{
+    public String Exception()
+        {
+
+           return "Division by zero is prohibited";
+        }
+}
